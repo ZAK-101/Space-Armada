20923a01a7d7e7231a37cf32a916ea5a49369272
dustinEsq <89570437+dustinEsq@users.noreply.github.com>
2022-07-26 14:44:39 +0800

    added load next scene function

---
+++ Space Armada/Assets/Scripts/homescreen.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class homescreen : MonoBehaviour
+{
+    public void loadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
