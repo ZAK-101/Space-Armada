5ff5e0567d1faa4b82d54f2eb964f3d227ef2ac6
dustinEsq <89570437+dustinEsq@users.noreply.github.com>
2022-07-26 16:52:35 +0800

    updated invader.cs

--- Space Armada/Assets/Scripts/Invader.cs
+++ Space Armada/Assets/Scripts/Invader.cs
@@ -49,8 +49,6 @@ public class Invader: MonoBehaviour
     public int enemiesLeft;
     public int waveValue;
 
-    public int totalAmountKilled { get { return amountKilled; } }
-
     // Start is called before the first frame update
     void Start()
     {
